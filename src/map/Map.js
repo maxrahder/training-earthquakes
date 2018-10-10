d4bc06416b553705436c599e8221f966d26ec295
Max Rahder <rahder@gmail.com>
2018-10-10 16:10:17 -0500

    Drawing markers using mocked data

--- src/map/Map.js
+++ src/map/Map.js
@@ -1,8 +1,17 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./Map.css";
+import Earthquakes from "../service/Earthquakes";
 
 export default class Map extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      earthquakes: []
+    };
+    debugger;
+    Earthquakes.get().then(this.drawMarkers.bind(this));
+  }
   pan(coordinate) {
     const ll = new window.google.maps.LatLng(
       coordinate.latitude,
@@ -18,6 +27,57 @@ export default class Map extends Component {
       });
 
     this._map.panTo(ll);
+  }
+
+  // @private
+  get markers() {
+    return this._markers || [];
+  }
+  set markers(markers) {
+    this._markers = markers;
+  }
+
+  // @private
+  drawMarkers(earthquakes) {
+    // Assert: the map has already been created
+
+    // Destroy the current set of markers.
+    this.markers.forEach(marker => {
+      marker.setMap(null);
+    });
+
+    this.markers = [];
+
+    // For each record, create and show a new marker, and push onto the array.
+    earthquakes.forEach(item => {
+      let ll = new window.google.maps.LatLng(item.latitude, item.longitude);
+      let marker = new window.google.maps.Marker({
+        position: ll,
+        icon: this.getMarker("red"),
+        item: item
+      });
+      this.markers.push(marker);
+      marker.setMap(this._map);
+    });
+
+    function attachListener(marker, record) {
+      window.google.maps.event.addListener(marker, "click", function() {
+        this.onSelected(record);
+      });
+    }
+  }
+
+  onSelected(item) {}
+
+  getMarker(color) {
+    return {
+      path: window.google.maps.SymbolPath.CIRCLE,
+      scale: 10,
+      strokeColor: "black",
+      strokeWeight: 3,
+      fillColor: color,
+      fillOpacity: 1.0
+    };
   }
 
   render() {
